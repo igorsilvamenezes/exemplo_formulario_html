23ccb7ca49c6a7634a12c7f3e900e3ce0dff4d7f
Igor Menezes <igorsilvamenezes@gmail.com>
2023-08-24 16:48:12 -0300

    Criacao do botao enviar e print dos valores dos campos do formulario

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Página carregada!!");
+console.log('Página carregada!!');
+
+document.addEventListener('submit', function(event){
+    //Evita a página ser recarregada ao submeter o formulário
+    event.preventDefault();
+
+    console.log('Formulario submetido!!!');
+
+    //Obtem o elemento html com ID fieldName
+    const edtName = document.getElementById('fieldName');
+    //Obtem o valor (texto digitado) do edit
+    const nameValue = edtName.value;
+    console.log('Nome:', nameValue);
+
+    //Obtem o elemento html com ID fieldAddress
+    const edtAddress = document.getElementById('fieldAddress');
+    //Obtem o valor (texto digitado) do edit
+    const addressValue = edtAddress.value;
+    console.log('Endereço:', addressValue);
+    
+    //Obtem o elemento html com ID fieldAge
+    const edtAge = document.getElementById('fieldAge');
+    //Obtem o valor (texto digitado) do edit
+    const ageValue = edtAge.value;
+    console.log('Idade:', ageValue);    
+});
